5747072099d92058291d0cd7950dd7bdc7557fd2
smandla <35283559+smandla@users.noreply.github.com>
2022-08-05 12:27:35 -0700

    post comment and vote

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const plantRoutes = require("./plant-routes");
+const commentRoutes = require("./comment-routes");
 
 router.use("/user", userRoutes);
 router.use("/plants", plantRoutes);
+router.use("/comments", commentRoutes);
 
 //commentRoutes
 //voteRoutes
