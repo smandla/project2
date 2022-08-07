d58f3aff8cc1f012540e904aa6e20e10bbfbb7e8
Jondo <chet9880@gmail.com>
2022-08-07 13:18:30 -0400

    fixed home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,22 @@ console.log("hello");
 
 /** TODO: Render plants to homepage */
 router.get("/", (req, res) => {
-  res.render("feed", {plants});
+  try {
+    const plantsData = await Plant.findAll({
+      include: [
+        {
+          model: Comment,
+          include: [{ model: Vote }, { model: User, attributes: ["username"] }],
+        },
+        { model: User, attributes: ["username"] },
+      ],
+    });
+    const plants = plantsData.map((plant) => plant.get({ plain: true }));
+    console.log("plants", plants);
+    res.render("feed", plants);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get("/login", (req, res) => {
